8d131c62e8988e1c8aa556330596c35ad060a40f
bulutluoz <abbulutluoz@gmail.com>
2024-08-30 08:25:27 +0200

    K01 sonu

---
+++ src/K1_programlamayaGiris/C01_HelloWorld.java
@@ -0,0 +1,21 @@
+package K1_programlamayaGiris;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Yorum yazmak icin // kullanilir
+        // Java yorum olarak yazilan satirlari
+        // CALISTIRMAZ
+        System.out.println(  "Hello World"  );
+
+        System.out.println( "Buraya ne yazarsak, Java konsola yazdirir");
+        /*
+           Birden fazla satiri
+           yorum satiri yapmak icin
+           * * kullanilabilir
+
+         */
+    }
+
+}
